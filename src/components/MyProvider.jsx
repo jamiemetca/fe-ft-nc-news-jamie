85ac1e4e20a808e8d16f8c9954d2dc4a00a50726
jamiemetca <jamiemetca@gmail.com>
2018-09-06 17:11:44 +0100

    updated logged in status

--- src/components/MyProvider.jsx
+++ src/components/MyProvider.jsx
@@ -5,16 +5,16 @@ export const MyContext = React.createContext();
 
 export class MyProvider extends Component {
   state = {
-    loggedIn: false,
-    user: {}
-    // user: {
-    //   _id: "5b40b6d3bfcb0f5d40a035fc",
-    //   username: "jessjelly",
-    //   name: "Jess Jelly",
-    //   avatar_url:
-    //     "https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg",
-    //   __v: 0
-    // }
+    loggedIn: true,
+    // user: {}
+    user: {
+      _id: "5b40b6d3bfcb0f5d40a035fc",
+      username: "jessjelly",
+      name: "Jess Jelly",
+      avatar_url:
+        "https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg",
+      __v: 0
+    }
   };
 
   render() {
